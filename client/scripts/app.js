259105f742f94ce46f566bf444fade57596b7ad6
Sourdoughh <jason.ly@gmail.com>
2015-02-17 12:27:12 -0800

    Fixed some bugs and added more functionality

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -6,33 +6,46 @@ var app = {
 $(document).ready(function(){
 
   app.init = function(){
-    setInterval(function(){
-      app.clearMessages();
-      app.fetch();
-    },5000);
     // listeners
-    $('.clear-button').on('click', function(){
-      app.clearMessages();
+
+    // populate room drop down with ALL possible rooms
+
+    $('.refresh-button').on('click', function(){
+      var roomselected = $('#roomSelect option:selected').val();
+      app.fetch(roomselected);
+
     });
+
     $('.add-message-button').on('click', function(){
       var msg = $('.add-message-input').val();
       app.addMessage(msg);
     });
+
     $('.add-room-button').on('click', function(){
       var room = $('.add-room-input').val();
-
       app.addRoom(room);
+      app.fetch(room);
     });
 
+    $('#roomSelect').change(function(){
+     var roomselected = $('#roomSelect option:selected').val();
+     app.fetch(roomselected);
+    });
+
+    app.constructRoomDropdown();
+    app.fetch();
   };
 
   app.addMessage = function(message) {
     // will eventually need to really add it
     // including room name
+
+   var roomselected = $('#roomSelect option:selected').val();
+
     var msgObj = {
-      'username': 'Steve Jobs',
+      'username': 'Spammer',
       'text': message,
-      'roomname': 'appleworks'
+      'roomname': roomselected
      };
     app.send(msgObj);
 
@@ -41,11 +54,17 @@ $(document).ready(function(){
     $('#chats').prepend(newChat);
   };
 
-  app.addRoom = function(room){
+  app.addRoom = function(room){ // does not go through a dup check
     var $roomOption = $('<option></option>');
+
+    room = app.cleanString(room);
+
     $roomOption.text(room).attr('value', room);
 
     $('#roomSelect').append($roomOption);
+
+    // change currently selected room?
+    app.fetch(room);
   };
 
   app.send = function(message){
@@ -66,13 +85,26 @@ $(document).ready(function(){
     });
   };
 
-  app.fetch = function(){
-    // &where={"results":{"roomname":"lobby"}}
+  app.fetch = function(where){
+    app.clearMessages();
+
+    var dataObj = {
+        order: '-createdAt',
+        limit: 100,
+    };
+
+    if (where) {
+      dataObj.where = '{"roomname":"' + where + '"}';
+    };
+
     $.ajax({
       // always use this url
-      url: app.server+'?order=-createdAt',
+      url: app.server,
+      data: dataObj,
       type: 'GET',
-      success: function(data){app.constructChats(data)},
+      success: function(data){
+        app.constructChats(data);
+      },
       error: function () {
         console.error('chatterbox: Failed to get messages');
       }
@@ -80,6 +112,7 @@ $(document).ready(function(){
   };
 
   app.constructChats = function(data){
+    // console.log(data);
     for(var i = 0; i < data.results.length; i++){
       if ( data.results[i].username !== undefined ||
            data.results[i].text !== undefined ) {
@@ -87,32 +120,56 @@ $(document).ready(function(){
         var username = data.results[i].username;
         var text = data.results[i].text;
         var room = data.results[i].roomname;
-        var timeStamp = data.results[i].createdAt;
-        var $chat = $('<div class="chat"></div>').text(username + ': ' + text + ' ' + timeStamp);
+        // var timeStamp = data.results[i].createdAt;
+        var $chat = $('<div class="chat"></div>').text(username + ': ' + text + ' ' + room);
+
+        // username = app.cleanString(username);
+        // text = app.cleanString(text);
+        // room = app.cleanString(room);
 
         $('#chats').append($chat);
-
-        if ( app.rooms.indexOf(room) === -1 && room ) {
-          app.rooms.push(room);
-
-          $('#roomSelect').empty();
-
-          for ( var j = 0; j < app.rooms.length; j++ ) {
-            app.addRoom(app.rooms[j]);
-          }
-        }
-
       }
     }
+  };
+
+  app.constructRoomDropdown = function(){
+    var dataObj = {
+        keys: 'roomname',
+        limit: 500
+    };
+
+    $.ajax({
+      url: app.server,
+      data: dataObj,
+      type: 'GET',
+      success: function(data){
+        console.log(data)
+        app.constructRooms(data);
+      },
+      error: function () {
+        console.error('chatterbox: Failed to get messages');
+      }
+    });
+  };
+
+  app.constructRooms = function(data){
+    for(var i = 0; i < data.results.length; i++){
+      var room = data.results[i].roomname;
+      if( app.rooms.indexOf(room) === -1 && room ) {
+        app.rooms.push(room);
+        app.addRoom(room);
+      }
+    }
+  };
+
+  app.cleanString = function(string) {
+    return string.replace( /[\|&;\$%@"<>\(\)\+,'\!\=\{\}\[\]]/g, "" );
   };
 
   app.clearMessages = function() {
     $('#chats').empty();
   };
 
-  // select username fn?
-  // where is the username coming from?
-  // add a clickable username...
 
   app.addFriend = function(){
 
@@ -122,13 +179,24 @@ $(document).ready(function(){
 
   };
 
-app.fetch();
+// USER NAME SELECTION
+// check current URL for a username?
+// input box to input/change username (show current username?)
+// button
+// event handler
+//
+  // select username fn?
+  // where is the username coming from?
+  // add a clickable username...
+
+
+
 app.init();
 
 
 /*
 - could store first chat element we prepend, and use
-  that unique id to inform our next GET request?
+  that unique id to inform our next fetch?
 
 - start with messages on screen vs waiting 5 seconds
 
