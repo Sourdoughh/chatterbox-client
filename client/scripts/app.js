7c51236fd0450102137744e4e451b626231341cf
Sourdoughh <jason.ly@gmail.com>
2015-02-16 16:38:34 -0800

    Finished up to fetch

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -2,53 +2,67 @@ var app = {};
 
 $(document).ready(function(){
 
-app.postToBoard = function(data){
-  for(var i = 0; i < data.results.length; i++){
-    var username = data.results[i].username;
-    // console.log(username)
-    var text = data.results[i].text;
-    console.log("This is the text: ", text)
+  app.server = 'https://api.parse.com/1/classes/chatterbox';
 
-    var chat = '<div>' + username + ' '+ text + '</div>';
+  app.init = function(){
+    //not sure what this will do
+  };
 
-    $('.chatterbox').html(chat);
-  }
-};
+  app.postToBoard = function(data){
+    for(var i = 0; i < data.results.length; i++){
+      // if no username/undefined, then what?
+      if ( data.results[i].username !== undefined ||
+           data.results[i].text !== undefined ) {
 
-app.init = function(){
-  //not sure what this will do
-};
+        // also, escape
+        var username = data.results[i].username;
+        console.log('this is the username: ',username)
 
-app.send = function(message){
-  //will make an ajax post to the server
-  $.ajax({
-    // always use this url
-    url: 'https://api.parse.com/1/classes/chatterbox',
-    type: 'POST',
-    data: JSON.stringify(message),
-    contentType: 'application/json',
-    success: function (data) {
-      console.log('chatterbox: Message sent');
-    },
-    error: function (data) {
-      // see: https://developer.mozilla.org/en-US/docs/Web/API/console.error
-      console.error('chatterbox: Failed to send message');
+        // if no text, then what?
+        // escape
+        var text = data.results[i].text;
+        // console.log("This is the text: ", text);
+
+        // var chat = '<div>' + username + ': '+ text + '</div>';
+        var $chat = $('<div class="chat"></div>').text(username + ': ' + text);
+
+
+        $('.chatterbox').append($chat);
+      }
+
     }
-  });
-};
+  };
 
-app.fetch = function(){
-  $.ajax({
-    // always use this url
-    url: 'https://api.parse.com/1/classes/chatterbox',
-    type: 'GET',
-    success: function(data){app.postToBoard(data)},
-    error: function () {
-      console.error('chatterbox: Failed to get messages');
-    }
-  });
+  app.send = function(message){
+    //will make an ajax post to the server
+    $.ajax({
+      // always use this url
+      url: app.server,
+      type: 'POST',
+      data: JSON.stringify(message),
+      contentType: 'application/json',
+      success: function (data) {
+        console.log('chatterbox: Message sent');
+      },
+      error: function (data) {
+        // see: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+        console.error('chatterbox: Failed to send message');
+      }
+    });
+  };
 
-};
+  app.fetch = function(){
+    $.ajax({
+      // always use this url
+      url: app.server,
+      type: 'GET',
+      success: function(data){app.postToBoard(data)},
+      error: function () {
+        console.error('chatterbox: Failed to get messages');
+      }
+    });
+  };
+    app.fetch();
 
 
 
